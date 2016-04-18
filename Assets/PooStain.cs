54dfc6f0b9d5e2fce9ff31e3b98d59c36421e738
gglang <gerald.lang@mail.mcgill.ca>
2016-04-18 18:48:15 -0400

    poo stain

---
+++ Assets/PooStain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PooStain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// derka derka
+		Debug.Log("DERKA DERKA");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//oops HACK
+		this.transform.position = this.transform.position + new Vector3(0.001f, 0f, 0f);
+	}
+}
